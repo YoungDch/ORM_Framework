b67ce7770f3e686fe305500233fd1e071f2f56cb
YoungDch <denis.cherkashin01@gmail.com>
2024-03-08 23:18:13 +0300

    git init

---
+++ src/main/java/ru/cherkashin/orm_framework/App.java
@@ -0,0 +1,13 @@
+package ru.cherkashin.orm_framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
